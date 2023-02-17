4871b39e6b0d5e6913823854a0fbd938bd87ba4e
shanellelck <shanellelck@outlook.com>
2023-02-17 14:02:33 -0700

    Initial commit
    Created the react app and set up the header for the 3 different pages

---
+++ shanelleli_launchpad-react-code-challenge/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="home">
+      <header>
+        <a href="./App.js">Home</a>
+        <ul>
+          <li><a href="./Universities.js">Universities</a></li>
+          <li><a href="./PostalLookup.js">Postal Lookup</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
