56bcebce62c461e3924b9bf986b6f8c173ae44eb
shanellelck <shanellelck@outlook.com>
2023-02-17 18:17:34 -0700

    Fixed the 3 pages

---
+++ shanelleli_launchpad-react-code-challenge/src/PostalLookup.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PostalLookup() {
+    return(
+        <div>Postal Lookup</div>
+    );
+}
+
+export default PostalLookup;
---
+++ shanelleli_launchpad-react-code-challenge/src/Universities.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Universities() {
+    return (
+        <div>Universities</div>
+    );
+}
+
+export default Universities;
