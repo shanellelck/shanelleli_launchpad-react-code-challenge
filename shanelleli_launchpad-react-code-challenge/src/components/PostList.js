1df9fee3d23d42665c63943393285f53aa7cdec9
shanellelck <shanellelck@outlook.com>
2023-02-24 00:58:11 -0700

    minor update to display a message when no post is found

--- shanelleli_launchpad-react-code-challenge/src/components/PostList.js
+++ shanelleli_launchpad-react-code-challenge/src/components/PostList.js
@@ -3,15 +3,20 @@ import PostInfo from './PostInfo';
 
 const PostList = ({ posts, postId }) => {
     const filteredPosts = postId ? posts.filter(post => post.id === parseInt(postId)) : posts;
+    
     return(
-        <ul className="post-list">
-            {filteredPosts.map(post => (
-                <li key={post.id}>
-                    <PostInfo post={post}/>
-                </li>
-            ))}
-        </ul>
+      <ul className="post-list">
+        {filteredPosts.length ? (
+          filteredPosts.map(post => (
+            <li key={post.id}>
+              <PostInfo post={post}/>
+            </li>
+          ))
+        ) : (
+          <li>No post found.</li>
+        )}
+      </ul>
     );
-}
+  }
 
 export default PostList;
